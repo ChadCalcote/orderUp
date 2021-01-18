82a0db5703d2a88978925fb4ae4c4f7f5b9a08b3
Chad Calcote <calcote@att.net>
2021-01-18 11:10:14 -0700

    Protect Main Page, Add Logout Button

--- app/routes/orders.py
+++ app/routes/orders.py
@@ -1,8 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import login_required
 
 bp = Blueprint("orders", __name__, url_prefix="")
 
 @bp.route('/')
+@login_required
 def index():
-    return "OrderUp!"
+    return render_template('orders.html')
 
