b6f61c48830b6eccd70691459293e90e005c4313
Chad Calcote <calcote@att.net>
2021-01-18 12:07:16 -0700

    Install Flask Migrate, Comment Out Models

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,23 @@
 from flask import Flask
 from .config import Configuration
 from .routes import orders, session
-from .models import db, Employee
+from .models import db  #, Employee
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(orders.bp)
 app.register_blueprint(session.bp)
 db.init_app(app)
+Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = "session.login"
 
 
-@login.user_loader
-def load_user(id):
-    return Employee.query.get(int(id))
+# @login.user_loader
+# def load_user(id):
+#     return Employee.query.get(int(id))
 
 
